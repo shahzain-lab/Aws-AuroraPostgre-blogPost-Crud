a3960353e1b48cab33358f2d03caa7d5f16a6bfe
Syed ZainUlAbideen <shahzain30196@gmail.com>
2022-04-22 13:38:15 +0500

    Initial commit

---
+++ test/serverless-aurora-mysql.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServerlessAuroraMysql from '../lib/serverless-aurora-mysql-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless-aurora-mysql-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServerlessAuroraMysql.ServerlessAuroraMysqlStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
